872f1740981fb6f3c0276af7e22951e186a4836d
Tao Pan <Pan25245>
2023-08-09 12:01:42 +0000

    Initial commit

--- factorial.c
+++ factorial.c
@@ -1,23 +1,12 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-
-// contains INT_MAX
 #include <limits.h>
-
-/* return the factorial of n.
- * The result is undefined if n < 0.
- *
- * The largest factorial that can be calculated depends
- * on your platform. On platforms where a `long`
- * is 8 bytes, the results of the function for
- * any n greater than 20 are undefined.
- */
+//my program
 long factorial(int n) {
   long result = 1;
-  for (int i = n; i >= 0; i--) {
-    result = result * i;
+  for (int i = n; i > 0; i--) {
+    result *= i;
   }
   return result;
 }
@@ -33,12 +22,11 @@ int main(int argc, char **argv) {
 
   char *end;
 
-  // clear errno so we can check whether strtol fails
   errno = 0;
   long n = strtol(argv[0], &end, 10);
   int res_errno = errno;
 
-  if (end == argv[0]) {
+  if (*end != '\0') {
     fprintf(stderr, "Error: couldn't interpret '%s' as a number\n", argv[0]);
     exit(1);
   } else if (res_errno == ERANGE) {
@@ -48,12 +36,11 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Error: '%s' is too big to fit in an int\n", argv[0]);
     exit(1);
   } else if (n < 0) {
-    fprintf(stderr, "Error: invalid value %ld:"
-            "factorial can only be calculated for non-negative numbers\n", n);
+    fprintf(stderr, "Error: invalid value %ld: factorial can only be calculated for non-negative numbers\n", n);
     exit(1);
   }
 
-  int nn = (short) n;
+  int nn = (int) n; // Use int instead of short here
   long result = factorial(nn);
   printf("factorial(%d) is %ld\n", nn, result);
 }
