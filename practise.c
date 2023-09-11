1b00566f7d76294421b394e1af262773ca51166a
Tao Pan <Pan25245>
2023-09-11 05:31:25 +0000

    Practising how to use git and running through the labs

---
+++ practise.c
@@ -0,0 +1,15 @@
+//CC=gcc CFLAGS="-std=c11 -pedantic -Wall -Wextra -Wshadow -Wconversion -O" make practise.o practise
+//./practise
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  uid_t ruid = getuid();
+  uid_t euid = geteuid();
+  printf("real uid is: %d\n", ruid);
+  printf("effective uid is: %d\n", euid);
+
+  return EXIT_SUCCESS;
+}
